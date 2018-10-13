b714a520d824da13018e00769efeb84c2c48dca5
LordKorea <lk97798@posteo.net>
2018-10-14 00:28:06 +0200

    added powered minecarts

---
+++ src/main/java/de/fuchspfoten/mokkit/entity/vehicle/minecart/MokkitPoweredMinecart.java
@@ -0,0 +1,30 @@
+package de.fuchspfoten.mokkit.entity.vehicle.minecart;
+
+import de.fuchspfoten.mokkit.MokkitServer;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.minecart.PoweredMinecart;
+
+import java.util.UUID;
+
+/**
+ * @see org.bukkit.entity.minecart.PoweredMinecart
+ */
+public class MokkitPoweredMinecart extends MokkitMinecart implements PoweredMinecart {
+
+    /**
+     * Constructor.
+     *
+     * @param server   The server this entity is in.
+     * @param location The location the entity is at.
+     * @param uuid     The UUID of the entity.
+     */
+    public MokkitPoweredMinecart(final MokkitServer server, final Location location, final UUID uuid) {
+        super(server, "PoweredMinecart", location, uuid);
+    }
+
+    @Override
+    public EntityType getType() {
+        return EntityType.MINECART_FURNACE;
+    }
+}
